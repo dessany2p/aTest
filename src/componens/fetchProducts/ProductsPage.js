945fe48867de1b7dde686890cde670f75cf174af
Mr.Green <roopxh@gmail.com>
2024-02-25 13:35:29 +0600

    status flag for fetch requests

--- src/componens/fetchProducts/ProductsPage.js
+++ src/componens/fetchProducts/ProductsPage.js
@@ -16,7 +16,6 @@ const filterDuplicates = (products) => {
 const fetchWithRetry = async (url, options, maxAttempts = 3) => {
    for (let attempt = 1; attempt <= maxAttempts; attempt++) {
       try {
-         // console.log(`Attempt ${attempt}`);
          const response = await fetch(url, options);
          if (!response.ok) throw new Error(`HTTP error! status: ${response.status}`);
          return await response.json();
@@ -28,12 +27,14 @@ const fetchWithRetry = async (url, options, maxAttempts = 3) => {
 };
 
 const ProductsPage = () => {
+   const [isLoading, setIsLoading] = useState(false);
    const [products, setProducts] = useState([]);
    const [page, setPage] = useState(0); // Управление текущей страницей
    const [filter, setFilter] = useState({}); // Фильтры: product, price, brand
 
    // Функция для загрузки товаров
    const fetchProducts = async (currentPage, currentFilter) => {
+      setIsLoading(true);
       const password = "Valantis";
       const timestamp = new Date().toISOString().split('T')[0].replace(/-/g, '');
       const authKey = CryptoJS.MD5(`${password}_${timestamp}`).toString();
@@ -62,6 +63,7 @@ const ProductsPage = () => {
          }
 
          const data = await responseIds.json();
+
          if (data && data.result) {
             return data.result;
          }
@@ -102,13 +104,13 @@ const ProductsPage = () => {
       try {
          const rawData = await fetchWithRetry('http://api.valantis.store:40000/', responseDetails);
          if (rawData && rawData.result) {
-            // console.log("Данные перед фильтрацией:", rawData.result.map(item => item.id));
             const filteredData = filterDuplicates(rawData.result);
-            // console.log("Данные после фильтрации:", filteredData.map(item => item.id));
             setProducts(filteredData); // Обновляем состояние продуктов детальной информацией
          }
       } catch (error) {
          console.error("Ошибка при получении данных о товарах:", error);
+      } finally {
+         setIsLoading(false)
       }
    };
 
@@ -126,22 +128,27 @@ const ProductsPage = () => {
    return (
       <div>
          <h2>Товары</h2>
-         <div>
-            <input name="product" placeholder="Название" onChange={handleFilterChange} />
-            <input name="price" placeholder="Цена" type="number" onChange={handleFilterChange} />
-            <input name="brand" placeholder="Бренд" onChange={handleFilterChange} />
-            <button onClick={() => fetchProducts(page, filter)}>Применить фильтры</button>
-         </div>
-         <div>
-            <button onClick={handlePrevPage}>Предыдущая страница</button>
-            <button onClick={handleNextPage}>Следующая страница</button>
-         </div>
-         <ul>
-            {products.map(({ id, product, price, brand }) => (
-               <li key={id}>{`ID: ${id}  ${product}  (${brand}) - ${price}₽`}</li>
-            ))}
-         </ul>
-
+         {isLoading ? (
+            <p>Загрузка...</p>
+         ) : (
+            <>
+               <div>
+                  <input name="product" placeholder="Название" onChange={handleFilterChange} />
+                  <input name="price" placeholder="Цена" type="number" onChange={handleFilterChange} />
+                  <input name="brand" placeholder="Бренд" onChange={handleFilterChange} />
+                  <button onClick={() => fetchProducts(page, filter)}>Применить фильтры</button>
+               </div>
+               <div>
+                  <button onClick={handlePrevPage}>Предыдущая страница</button>
+                  <button onClick={handleNextPage}>Следующая страница</button>
+               </div>
+               <ul>
+                  {products.map(({ id, product, price, brand }) => (
+                     <li key={id}>{`ID: ${id}  ${product}  (${brand}) - ${price}₽`}</li>
+                  ))}
+               </ul>.
+            </>
+         )}
       </div>
    );
 };
